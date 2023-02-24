59603ea0a305f0df81938ae28909339ed1d6770b
LucLucero <lucianoxlucero@hotmail.com>
2023-02-24 10:39:35 -0300

    node

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var app = require('./config/server');
-var homeRota = require('./app/routes/home');
-homeRota(app);
+var homeRota = require('./app/routes/home')(app);
 
-var noticiasRota = require('./app/routes/noticias');
-noticiasRota(app);
 
-var formRota = require('./app/routes/formulario_inclusao_noticia');
-formRota(app);
+var noticiasRota = require('./app/routes/noticias')(app);
+
+
+var formRota = require('./app/routes/formulario_inclusao_noticia')(app);
+
 
 
 app.listen(3000, function(){
