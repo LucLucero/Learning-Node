72e919e2d6b2655540ddfa61d6d4328de131c30e
LucLucero <lucianoxlucero@hotmail.com>
2023-02-24 10:14:14 -0300

    Curso Node

---
+++ app.js
@@ -0,0 +1,25 @@
+app.get('/', function (req,res) {
+    
+    res.render('home/index');
+
+});
+app.get('/formulario_inclusao_noticia', function (req,res) {
+    
+    res.render('admin/form_add_noticia');
+
+});
+app.get('/noticias', function (req,res) {
+    
+    res.render('noticias/noticias');
+
+});
+
+
+
+
+app.listen(3000, function(){
+
+    console.log("Server ON");
+    
+
+});
