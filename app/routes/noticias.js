ab8324b0b23398c7bd86cb52f587acd571bd17ad
LucLucero <lucianoxlucero@hotmail.com>
2023-03-01 16:10:51 -0300

    fixing bug, news dont show at browser

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,28 +1,25 @@
 function newsToRender(app) {
-    
+
     app.get('/noticias', function (req,res) {
-    var mysql = require('mysql');
+        var mysql = require('mysql');
 
-    //create connection utiliza os parametros em json (chave, valor)
-    var connection = mysql.createConnection({
+        //create connection utiliza os parametros em json (chave, valor)
+        var connection = mysql.createConnection({
 
-        host : 'localhost', //endereço do servidor neste caso o localhost
-        user : 'root',
-        password : '3922765lu',
-        database : 'portal_noticias'
+            host : 'localhost', //endereço do servidor neste caso o localhost
+            user : 'root',
+            password : '3922766',
+            database : 'portal_noticias'
 
-    });
-    //query espera dois parametros: QUERY SQL , func callback
-    //a func callback tbm demanda dois paramentros, o primeiro é o error e o segundo o resultado a ser tratado.
-    connection.query("select *  from noticias", function(error,result){
+        });
 
-        res.send(result);
-        
-    });
-    
-    
-        
-        res.render('noticias/noticias');
+        //query espera dois parametros: QUERY SQL , func callback
+        //a func callback tbm demanda dois paramentros, o primeiro é o error e o segundo o resultado a ser tratado.
+        connection.query('select * from noticias', function(error,result){
+            res.send(result);    
+                       
+        });
+            //res.render('noticias/noticias');
     
     });
 }
