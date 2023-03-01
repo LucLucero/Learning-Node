5d99ab76e67eb2be5d2c6af103e7d8df77c9fb0c
LucLucero <lucianoxlucero@hotmail.com>
2023-03-01 16:59:18 -0300

    config consign

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = require('./config/server');
-var homeRota = require('./app/routes/home')(app);
-var noticiasRota = require('./app/routes/noticias')(app);
-var formRota = require('./app/routes/formulario_inclusao_noticia')(app);
+// //var homeRota = require('./app/routes/home')(app);
+// var noticiasRota = require('./app/routes/noticias')(app);
+// var formRota = require('./app/routes/formulario_inclusao_noticia')(app);
 app.listen(3000, function(){
     console.log("Server ON"); 
 });
